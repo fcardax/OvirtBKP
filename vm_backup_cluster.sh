833072969d54be343667f5518c5b6569d19bd177
f.cardarello@gmail.com <f.cardarello@gmail.com>
2022-10-03 12:15:38 +0200

    fix cluter backup

--- vm_backup_cluster.sh
+++ vm_backup_cluster.sh
@@ -4,11 +4,8 @@ source functions.sh
 
 CLUSTER_NAME=$1
 CLUSTER_ID=$(get_cluster_id $CLUSTER_NAME)
-WDIR=/root/bash_snapshots
 
-#[ ! -z $1 ] && LIST=$(get_all_vms | egrep -e $1)
-#[ -z $1 ] && LIST=$(get_all_vms)
 for I in $(get_all_vms_cluster $CLUSTER_ID)
 do
-	echo $WDIR/vm_backup.sh $I 
+	echo $PWD/vm_backup.sh $I 
 done
